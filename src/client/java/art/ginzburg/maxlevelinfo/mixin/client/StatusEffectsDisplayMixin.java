ab3db45bb9eb8a15f7ec7d4e691306efab03c83e
Art Ginzburg <ginzburg.arthur@gmail.com>
2025-02-11 22:01:24 +0400

    feat: show Max Level on effects in the inventory

---
+++ src/client/java/art/ginzburg/maxlevelinfo/mixin/client/StatusEffectsDisplayMixin.java
@@ -0,0 +1,38 @@
+package art.ginzburg.maxlevelinfo.mixin.client;
+
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.gui.screen.ingame.StatusEffectsDisplay;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.screen.ScreenTexts;
+import net.minecraft.text.Text;
+import art.ginzburg.maxlevelinfo.util.PotionUtils;
+
+@Mixin(StatusEffectsDisplay.class)
+public class StatusEffectsDisplayMixin {
+
+  @Inject(method = "getStatusEffectDescription", at = @At("TAIL"))
+  public void overrideGetStatusEffectDescription(StatusEffectInstance statusEffect, CallbackInfoReturnable<Text> cir) {
+
+    int maxAmplifier = PotionUtils.getMaxAmplifierForEffect(statusEffect);
+    if (maxAmplifier <= 0 || statusEffect.getAmplifier() == maxAmplifier)
+      return;
+
+    List<Text> siblings = cir.getReturnValue().getSiblings();
+
+    if (maxAmplifier > 0 && statusEffect.getAmplifier() == 0) {
+      siblings.add(ScreenTexts.SPACE);
+      siblings.add(Text.translatable("enchantment.level." + 1));
+    }
+
+    siblings.add(ScreenTexts.SPACE);
+    siblings.add(Text.literal("/"));
+    siblings.add(ScreenTexts.SPACE);
+    siblings.add(Text.translatable("enchantment.level." + (maxAmplifier + 1)));
+  }
+}
---
+++ src/client/java/art/ginzburg/maxlevelinfo/util/PotionUtils.java
@@ -0,0 +1,71 @@
+package art.ginzburg.maxlevelinfo.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import net.minecraft.component.type.OminousBottleAmplifierComponent;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.potion.Potion;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
+
+public class PotionUtils {
+  private static final Map<StatusEffectKey, Integer> effectToMaxAmplifierCache = new HashMap<>();
+  private static boolean cacheInitialized = false;
+
+  private static void initializeCache() {
+    if (cacheInitialized)
+      return;
+    cacheInitialized = true;
+
+    for (Potion potion : Registries.POTION) {
+      RegistryEntry<Potion> potionEntry = Registries.POTION.getEntry(potion);
+      if (potionEntry == null)
+        continue;
+
+      for (StatusEffectInstance effectInstance : potion.getEffects()) {
+        StatusEffectKey key = new StatusEffectKey(effectInstance);
+
+        effectToMaxAmplifierCache.merge(key, effectInstance.getAmplifier(), Math::max);
+      }
+    }
+
+    effectToMaxAmplifierCache.put(new StatusEffectKey(StatusEffects.BAD_OMEN),
+        OminousBottleAmplifierComponent.MAX_VALUE);
+  }
+
+  public static int getMaxAmplifierForEffect(StatusEffectInstance effect) {
+    initializeCache();
+    return effectToMaxAmplifierCache.getOrDefault(new StatusEffectKey(effect), -1);
+  }
+
+  private static class StatusEffectKey {
+    private final String effectId;
+
+    public StatusEffectKey(StatusEffectInstance effectInstance) {
+      this.effectId = effectInstance.getEffectType().getIdAsString();
+    }
+
+    public StatusEffectKey(RegistryEntry<StatusEffect> effect) {
+      this.effectId = effect.getIdAsString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (!(obj instanceof StatusEffectKey))
+        return false;
+      StatusEffectKey other = (StatusEffectKey) obj;
+      return effectId.equalsIgnoreCase(other.effectId);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(effectId);
+    }
+  }
+}
