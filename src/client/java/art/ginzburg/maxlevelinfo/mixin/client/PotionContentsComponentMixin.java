2e7eb44d3cd4f9c563c2c5392407675b73461fa2
Art Ginzburg <ginzburg.arthur@gmail.com>
2025-02-15 05:55:22 +0400

    fix: add vanilla "When Applied" texts to potions

--- src/client/java/art/ginzburg/maxlevelinfo/mixin/client/PotionContentsComponentMixin.java
+++ src/client/java/art/ginzburg/maxlevelinfo/mixin/client/PotionContentsComponentMixin.java
@@ -5,9 +5,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.google.common.collect.Lists;
+import com.mojang.datafixers.util.Pair;
+
+import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.component.type.OminousBottleAmplifierComponent;
 import net.minecraft.component.type.PotionContentsComponent;
-
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffectUtil;
@@ -15,7 +20,7 @@ import net.minecraft.entity.effect.StatusEffectUtil;
 import net.minecraft.potion.Potion;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntry;
-
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -66,27 +71,36 @@ public class PotionContentsComponentMixin {
   private static void modifyPotionTooltip(Iterable<StatusEffectInstance> effects, Consumer<Text> textConsumer,
       float durationMultiplier, float tickRate, CallbackInfo ci,
       String potionOriginalName, boolean isBadOmen) {
+    List<Pair<RegistryEntry<EntityAttribute>, EntityAttributeModifier>> list = Lists
+        .<Pair<RegistryEntry<EntityAttribute>, EntityAttributeModifier>>newArrayList();
     ci.cancel();
 
     Optional<Potion> potionLongVersion = getPotionVersion("long_" + potionOriginalName);
     Optional<Potion> potionStrongVersion = getPotionVersion("strong_" + potionOriginalName);
 
-    // `bl` in Yarn mappings
-    boolean isEmpty = true;
+    boolean isEmpty = true; // `bl` in Yarn mappings
 
     for (StatusEffectInstance effect : effects) {
+      // #region This is taken 1:1 (variable names changed though) from buildTooltip
+      // in PotionContentsComponent
       isEmpty = false;
       MutableText effectText = Text.translatable(effect.getTranslationKey());
       RegistryEntry<StatusEffect> registryEntry = effect.getEffectType();
+      registryEntry.value().forEachAttributeModifier(effect.getAmplifier(),
+          (attribute, modifier) -> list.add(new Pair<>(attribute, modifier)));
+      // #endregion
 
       if (potionLongVersion.isPresent()) {
         effectText.append(Text.literal(isEffectProlonged(effect, potionLongVersion) ? "+" : "-"));
       }
 
+      // #region This is taken 1:1 (variable names changed though) from buildTooltip
+      // in PotionContentsComponent
       if (effect.getAmplifier() > 0) {
         effectText = Text.translatable("potion.withAmplifier", effectText,
             Text.translatable("potion.potency." + effect.getAmplifier()));
       }
+      // #endregion
 
       if (isBadOmen) {
         appendBadOmenInfo(effect, effectText);
@@ -97,18 +111,63 @@ public class PotionContentsComponentMixin {
         effectText.append(Text.literal(" I"));
       }
 
+      // #region This is taken 1:1 (variable names changed though) from buildTooltip
+      // in PotionContentsComponent
       if (!effect.isDurationBelow(20)) {
         effectText = Text.translatable("potion.withDuration", effectText,
             StatusEffectUtil.getDurationText(effect, durationMultiplier, tickRate));
       }
 
       textConsumer.accept(effectText.formatted(registryEntry.value().getCategory().getFormatting()));
+      // #endregion
     }
 
+    // #region This is taken 1:1 (variable names changed though) from buildTooltip
+    // in PotionContentsComponent
     if (isEmpty) {
       textConsumer.accept(NONE_TEXT);
     }
 
+    addVanillaTextsAKAWhenApplied(textConsumer, list);
+    // #endregion
+  }
+
+  /** This is taken 1:1 from buildTooltip in {@link PotionContentsComponent} */
+  private static void addVanillaTextsAKAWhenApplied(Consumer<Text> textConsumer,
+      List<Pair<RegistryEntry<EntityAttribute>, EntityAttributeModifier>> list) {
+    if (!list.isEmpty()) {
+      textConsumer.accept(ScreenTexts.EMPTY);
+      textConsumer.accept(Text.translatable("potion.whenDrank").formatted(Formatting.DARK_PURPLE));
+
+      for (Pair<RegistryEntry<EntityAttribute>, EntityAttributeModifier> pair : list) {
+        EntityAttributeModifier entityAttributeModifier = pair.getSecond();
+        double d = entityAttributeModifier.value();
+        double e;
+        if (entityAttributeModifier.operation() != EntityAttributeModifier.Operation.ADD_MULTIPLIED_BASE
+            && entityAttributeModifier.operation() != EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL) {
+          e = entityAttributeModifier.value();
+        } else {
+          e = entityAttributeModifier.value() * 100.0;
+        }
+
+        if (d > 0.0) {
+          textConsumer.accept(
+              Text.translatable(
+                  "attribute.modifier.plus." + entityAttributeModifier.operation().getId(),
+                  AttributeModifiersComponent.DECIMAL_FORMAT.format(e),
+                  Text.translatable(pair.getFirst().value().getTranslationKey()))
+                  .formatted(Formatting.BLUE));
+        } else if (d < 0.0) {
+          e *= -1.0;
+          textConsumer.accept(
+              Text.translatable(
+                  "attribute.modifier.take." + entityAttributeModifier.operation().getId(),
+                  AttributeModifiersComponent.DECIMAL_FORMAT.format(e),
+                  Text.translatable(pair.getFirst().value().getTranslationKey()))
+                  .formatted(Formatting.RED));
+        }
+      }
+    }
   }
 
   private static boolean isEffectProlonged(StatusEffectInstance effect, Optional<Potion> potionLongVersion) {
